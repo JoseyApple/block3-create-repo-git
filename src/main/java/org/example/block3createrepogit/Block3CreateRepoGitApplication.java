0c53a411dbbeba5d1164d94fe174b18e8038e06d
JoseyApple <josemak87@hotmail.es>
2025-02-14 09:50:50 +0100

    Primer commit

---
+++ src/main/java/org/example/block3createrepogit/Block3CreateRepoGitApplication.java
@@ -0,0 +1,13 @@
+package org.example.block3createrepogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block3CreateRepoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block3CreateRepoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/block3createrepogit/Block3CreateRepoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.block3createrepogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block3CreateRepoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
